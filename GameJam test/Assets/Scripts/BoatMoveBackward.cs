1e66a699906fab5c8e667fd5db7da096fa793d33
Yipossible <yi.wang.carnegie.mellon@gmail.com>
2017-01-20 23:19:41 -0500

    first push

---
+++ GameJam test/Assets/Scripts/BoatMoveBackward.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatMoveBackward : MonoBehaviour {
+
+	public Rigidbody boat;
+	// Use this for initialization
+	void Start () {
+		boat = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float pullSpeed = -1f;
+		//boat.AddForce(transform.right * pullSpeed);
+		boat.velocity = new Vector3 (-1 ,0,0);
+
+}
+		
+
+	}
+
+
+
+
---
+++ GameJam test/Assets/Scripts/TypeToGo.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TypeToGo : MonoBehaviour {
+
+	private BoatMoveBackward boatMoveBackward;
+
+	public Rigidbody boat;
+	// Use this for initialization
+	void Start () {
+		var input = gameObject.GetComponent<InputField> ();
+		var boatMoveBackward = GetComponent<BoatMoveBackward> ();
+		var typeEvent = new InputField.SubmitEvent ();
+		typeEvent.AddListener (GoForward);
+		input.onEndEdit = typeEvent;
+	}
+
+	// Update is called once per frame
+	void GoForward (string input) {
+		Debug.Log ("user type" + input);
+		boat = boatMoveBackward.GetComponent<Rigidbody> ();
+		if ( input.Contains("hello"))
+			
+		boat.transform.Translate (Time.deltaTime * 50, 0, 0);
+	}
+}
